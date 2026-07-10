861c84cf5201c672442e5f205e958f1d39365430
Dat Nguyen <datnguyen@Dat-Nguyen-MacBook-Pro.local>
2026-07-10 10:10:21 +0700

    init db

--- content-services/server.js
+++ content-services/server.js
@@ -1,13 +1,15 @@
-const express = require('express'); // import express tool
+const express = require('express');
+const mongoose = require('mongoose');
 
-const app = express(); // init app
+const app = express();
+const port = 5001;
 
-const PORT = 5001; // define port that server will run on
+app.use(express.json()); // allow Express to read JSON data from front-end
 
-app.get('/api/test', (req, res) => {
-    res.json({ message: 'Hello World' })
-})
+//connect to MONGOOSE
+mongoose.connect('mongodb://content_db:27017/content_db')
+  .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
+  .catch(err => console.log("Cannot connect to database:", err));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
