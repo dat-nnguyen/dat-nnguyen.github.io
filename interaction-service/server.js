2004f3ae2c9c64ed4d7c6c45f1785635648f97dc
dat-nnguyen <tuandat.nguyen.dev@gmail.com>
2026-07-11 08:11:58 +0700

    init interaction-service

---
+++ interaction-service/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const { Pool } = require('pg'); // PostgreSQL driver
+const PORT = 5002;
+const app = express();
+
+app.use(express.json()); // allow Express to read JSON data from front-end
+
+// connect to PostgreSQL database
+const pool = new Pool({
+  user: 'admin',
+  host: 'localhost',
+  database: 'interaction_db',
+  password: 'secretpassword',
+  port: 5432,
+});
+
+// testing connection to database
+pool.connect()
+  .then(() => {
+    console.log('Connected successfully to PostgreSQL database');
+  })
+  .catch(err => console.log(err))
+// Init table
+const initDb = async () => {
+  const createTableText = `
+        CREATE TABLE IF NOT EXISTS comments (
+            id SERIAL PRIMARY KEY,
+            article_id VARCHAR(255) NOT NULL,
+            author_name VARCHAR(100) NOT NULL,
+            author_email VARCHAR(255) NOT NULL,
+            content TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    `;
+  try {
+    await pool.query(createTableText);
+    console.log('Comments table is ready.');
+  } catch (err) {
+    console.error('Error creating table:', err);
+  }
+};
+initDb();
+
+app.get('/api/test', (req, res) => {
+  res.json({ message: 'Hello from the interaction service!' });
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
