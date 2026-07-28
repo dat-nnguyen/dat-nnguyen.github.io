af00905c3fca05ed3583661d13aa72ff6350d965
dat-nnguyen <datnguyen3142@gmail.com>
2026-07-28 15:45:45 +0700

    Fix Vite script tag bundling for app.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  root: 'frontend',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
   server: {
     proxy: {
       '/api': {
